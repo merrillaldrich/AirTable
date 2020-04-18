df763069ca25b02f84a898d4b2a0ffc06be3a484
merrillaldrich <maldrich@blue-granite.com>
2020-04-18 12:13:54 -0500

    Add second wall

--- PhysicsExperiment/Form1.cs
+++ PhysicsExperiment/Form1.cs
@@ -26,18 +26,18 @@ namespace PhysicsExperiment
             PhysicsTimer.Enabled = true;
         }
 
-        Item B1 = new Item(100,260,15,0.25F,new Velocity(20,10),0.1F);
+        Item B1 = new Item(100,260,15,0.25F,new Velocity(20,15),0.1F);
         Wall W1 = new Wall(new Rectangle(700, 10, 10, 440));
+        Wall W2 = new Wall(new Rectangle(10, 10, 700, 10));
 
-        Pen linePen = new Pen(Color.DarkGray, 1);
         Pen wallPen = new Pen(Color.DarkGray, 1) { Alignment = PenAlignment.Inset };
         Pen B1Pen = new Pen(Color.DarkBlue, 2) { Alignment = PenAlignment.Inset };
 
         private void DrawingPanel_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
-            e.Graphics.DrawLine(linePen, 10, 220, DrawingPanel.Width - 10, 220);
             e.Graphics.DrawRectangle(wallPen, W1.Extents);
+            e.Graphics.DrawRectangle(wallPen, W2.Extents);
             e.Graphics.DrawCircle(B1Pen, B1.Position.X, B1.Position.Y, B1.Radius);
         }
 
@@ -59,28 +59,24 @@ namespace PhysicsExperiment
                 if( B1.Position.X + B1.Radius >= W1.Extents.X)
                 {
 
-                    // For the 2D version, change the direction property of the 
-                    // velocity to 180 degrees different, and reduce the speed
-                    // property to mimic friction
-
-                    // B1.V.Direction += 180;
-
-                    // Demo explicit calculation of the angle of reflection
-
-                    // float WallAngle = W1.Angle;
-                    // float moveTo0 = B1.V.Direction - WallAngle;
-                    // float reflect = moveTo0 * -1;
-                    // float moveBack = reflect + WallAngle;
-                    // B1.V.Direction = moveBack;
-
-                    // With some algebra this simplifies to
-                    // B1.V.Direction = -(B1.V.Direction - W1.Angle) + W1.Angle;
-
+                    // Compute the new direction our item is heading based on angle of incidence
                     B1.V.Direction = W1.Angle - (B1.V.Direction - W1.Angle);
-
                     B1.V.Speed = B1.V.Speed * 0.8f;
+
                 }
             }
+            if (B1.BoundingBox().IntersectsWith(W2.BoundingBox()))
+            {
+                if (B1.Position.Y - B1.Radius <= W2.Extents.Y + W2.Extents.Height)
+                {
+
+                    // Compute the new direction our item is heading based on angle of incidence
+                    B1.V.Direction = W2.Angle - (B1.V.Direction - W2.Angle);
+                    B1.V.Speed = B1.V.Speed * 0.8f;
+
+                }
+            }
+
         }
     }
 }
